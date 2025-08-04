204654438e5299ae3f5fa65742d28a1406d87e34
bam <juhyun19981129@gmail.com>
2025-08-04 20:53:27 +0900

    first commit

---
+++ src/main/java/com/joo/scalemall/ScalemallApplication.java
@@ -0,0 +1,13 @@
+package com.joo.scalemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalemallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalemallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joo/scalemall/ScalemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.scalemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalemallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
