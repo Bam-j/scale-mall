2db9ba835d86a61a0f07d53808038ff889d0d628
bam <juhyun19981129@gmail.com>
2025-08-07 19:05:58 +0900

    feat: 상품 상세 페이지 이동 컨트롤러 메소드 추가

--- src/main/java/com/joo/scalemall/controller/PageViewController.java
+++ src/main/java/com/joo/scalemall/controller/PageViewController.java
@@ -3,11 +3,13 @@ package com.joo.scalemall.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joo.scalemall.dto.ProductResponse;
+import com.joo.scalemall.service.ProductCacheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
 
@@ -16,9 +18,11 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class PageViewController {
 
+    private final ProductCacheService productCacheService;
     private final ReactiveRedisTemplate reactiveRedisTemplate;
     private final ObjectMapper objectMapper;
 
+    //메인 홈페이지
     @GetMapping
     public String index(Model model){
         String key = "product:1";
@@ -38,5 +42,13 @@ public class PageViewController {
         ProductResponse productResponse = productResponseMono.block();
         model.addAttribute("product", productResponse);
         return "index";
+    }
+
+    //상품 상세 페이지
+    @GetMapping("/detail/{id}")
+    public String detailPage(@PathVariable Long id, Model model) {
+        ProductResponse productResponse = productCacheService.getProductFromRedis(id).block();
+        model.addAttribute("product", productResponse);
+        return "detailPage";
     }
 }
