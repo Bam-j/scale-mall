5f89c6fa76be9d955177b51a4ae48384b730b67b
bam <juhyun19981129@gmail.com>
2025-08-11 21:33:54 +0900

    feat: 구매 로직 추가

--- src/main/java/com/joo/scalemall/controller/ProductController.java
+++ src/main/java/com/joo/scalemall/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.joo.scalemall.controller;
 
 import com.joo.scalemall.dto.PurchaseRequest;
+import com.joo.scalemall.service.ProductCacheService;
 import com.joo.scalemall.service.PurchaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/products")
@@ -17,8 +19,7 @@ public class ProductController {
     private final PurchaseService purchaseService;
 
     @PostMapping("/purchase")
-    public ResponseEntity<String> purchase(@RequestBody PurchaseRequest purchaseRequest) {
-        purchaseService.purchase(purchaseRequest);
-        return ResponseEntity.ok("구매 성공");
+    public Mono<ResponseEntity<String>> purchase(@RequestBody PurchaseRequest purchaseRequest) {
+        return purchaseService.purchase(purchaseRequest);
     }
 }
--- src/main/java/com/joo/scalemall/service/PurchaseService.java
+++ src/main/java/com/joo/scalemall/service/PurchaseService.java
@@ -1,14 +1,31 @@
 package com.joo.scalemall.service;
 
 import com.joo.scalemall.dto.PurchaseRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class PurchaseService {
 
-    public void purchase(PurchaseRequest purchaseRequest) {
+    private final ProductCacheService productCacheService;
+
+    public Mono<ResponseEntity<String>> purchase(PurchaseRequest purchaseRequest) {
         Long id = purchaseRequest.id();
 
-        //TODO: 재고 감소, 중복 구매 방지
+        return productCacheService.decrementStockSafely(id)
+            .map(result -> {
+                switch (result) {
+                    case SUCCESS:
+                        return ResponseEntity.ok("구매 성공");
+                    case OUT_OF_STOCK:
+                        return ResponseEntity.status(409).body("품절");
+                    case NO_STOCK_KEY:
+                    default:
+                        return ResponseEntity.status(500).body("재고 키 없음");
+                }
+            });
     }
 }
